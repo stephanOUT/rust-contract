3f0c14899661c84f86e35547fcdb91cbaa418d81
MoneyCollector <moneycollectorx@gmail.com>
2024-01-25 18:22:56 +0100

    Removed unused errors from error.rs

--- src/error.rs
+++ src/error.rs
@@ -9,23 +9,8 @@ pub enum ContractError {
     #[error("Unauthorized")]
     Unauthorized {},
 
-    #[error("Custom Error val: {val:?}")]
-    CustomError { val: String },
-
-    #[error("Not found")]
-    NotFound,
-    
-    #[error("Serialization")]
-    SerializationError,
-
     #[error("Trading is disabled")]
     TradingIsDisabled {},
-
-    #[error("Buy/Sell quantity limit exceeded")]
-    BuySellQuantityLimitExceeded {},
-
-    #[error("Insufficient payment")]
-    InsufficientPayment {},
 
     #[error("Invalid token sent")]
     InvalidTokenSentPayment {},
