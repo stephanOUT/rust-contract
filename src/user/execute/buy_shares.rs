b5c8cbb8821d9d10571ce0cc2dee875216b3def7
Neer Patel <neer@out.app>
2024-01-25 12:14:40 -0500

    add address validation to buy_shares

--- src/user/execute/buy_shares.rs
+++ src/user/execute/buy_shares.rs
@@ -26,18 +26,21 @@ pub fn buy_shares(
     shares_subject: Addr,
     referral: Addr,
 ) -> Result<Response, ContractError> {
+    let validated_referral_address = deps.api.addr_validate(&referral.to_string())?;
+    let validated_shares_subject_address = deps.api.addr_validate(&shares_subject.to_string())?;
+
     let state = STATE.load(deps.storage)?;
 
     let shares_supply = Uint128::new(1) + SHARES_SUPPLY
-        .may_load(deps.storage, &shares_subject)?
+        .may_load(deps.storage, &validated_shares_subject_address)?
         .unwrap_or_default();
 
     let shares_balance = SHARES_BALANCE
-        .may_load(deps.storage, (&info.sender, &shares_subject))?
+        .may_load(deps.storage, (&info.sender, &validated_shares_subject_address))?
         .unwrap_or_default();
 
     let shares_holders = SHARES_HOLDERS
-        .may_load(deps.storage, &shares_subject)?
+        .may_load(deps.storage, &validated_shares_subject_address)?
         .unwrap_or_default();
 
     let price = get_price(shares_supply);
@@ -51,7 +54,7 @@ pub fn buy_shares(
         assert!(info.funds[0].amount >= total, "Insufficient payment");
         SHARES_BALANCE.update(
             deps.storage,
-            (&info.sender, &shares_subject),
+            (&info.sender, &validated_shares_subject_address),
             |balance: Option<Uint128>| -> StdResult<_> {
                 Ok(balance.unwrap_or_default() + Uint128::new(1))
             },
@@ -59,7 +62,7 @@ pub fn buy_shares(
 
         SHARES_SUPPLY.update(
             deps.storage,
-            &shares_subject,
+            &validated_shares_subject_address,
             |supply: Option<Uint128>| -> StdResult<_> {
                 Ok(supply.unwrap_or_default() + Uint128::new(1))
             },
@@ -67,7 +70,7 @@ pub fn buy_shares(
 
         // If is first buy, add as a holder
         if shares_balance.is_zero() {
-            increment_share_holders(deps, shares_subject.clone())?;
+            increment_share_holders(deps, validated_shares_subject_address.clone())?;
         }
 
         let protocol_fee_result = BankMsg::Send {
@@ -76,11 +79,11 @@ pub fn buy_shares(
         };
 
         let subject_fee_result = BankMsg::Send {
-            to_address: shares_subject.to_string(),
+            to_address: validated_shares_subject_address.to_string(),
             amount: coins(subject_fee.into(), OUT_DENOM),
         };
         let referral_fee_result = BankMsg::Send {
-            to_address: referral.to_string(),
+            to_address: validated_referral_address.to_string(),
             amount: coins(referral_fee.into(), OUT_DENOM),
         };
 
@@ -96,13 +99,13 @@ pub fn buy_shares(
                 .add_event(
                     Event::new("buy_shares")
                         .add_attribute("sender", info.sender)
-                        .add_attribute("shares_subject", shares_subject)
+                        .add_attribute("shares_subject", validated_shares_subject_address)
                         .add_attribute("amount", Uint128::new(1))
                         .add_attribute("shares_balance_new", shares_balance_new)
                         .add_attribute("shares_supply_new", shares_supply + Uint128::new(1))
                         .add_attribute("subject_fees", subject_fee)
                         .add_attribute("referral_fees", referral_fee)
-                        .add_attribute("referral", referral)
+                        .add_attribute("referral", validated_referral_address)
                         .add_attribute("total", total)
                         .add_attribute("funds", info.funds[0].amount),
                 )
@@ -118,13 +121,13 @@ pub fn buy_shares(
             .add_event(
                 Event::new("buy_shares")
                     .add_attribute("sender", info.sender)
-                    .add_attribute("shares_subject", shares_subject)
+                    .add_attribute("shares_subject", validated_shares_subject_address)
                     .add_attribute("amount", Uint128::new(1))
                     .add_attribute("shares_balance_new", shares_balance_new)
                     .add_attribute("shares_supply_new", shares_supply + Uint128::new(1))
                     .add_attribute("subject_fees", subject_fee)
                     .add_attribute("referral_fees", referral_fee)
-                    .add_attribute("referral", referral)
+                    .add_attribute("referral", validated_referral_address)
                     .add_attribute("total", total)
                     .add_attribute("funds", info.funds[0].amount),
             )
